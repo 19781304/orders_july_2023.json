4f785fd7bd2a94b3bf2309f091f742754b6e8ed1
19781304 <nata.zaika.1111@gmail.com>
2025-01-19 18:17:30 +0700

    Update july_2023.py

--- july_2023.py
+++ july_2023.py
@@ -60,13 +60,23 @@ else:
         total_orders += 1
         total_items += items_count
 
+           # Находим день с максимальным количеством заказов
+    most_orders_day = max(orders_per_day, key=orders_per_day.get)
+
+    # Находим пользователя с максимальным количеством заказов
+    most_orders_user = max(user_orders, key=user_orders.get)
+
+    # Находим пользователя с максимальной суммарной стоимостью заказов
+    top_spending_user = max(user_total_spent, key=user_total_spent.get)
+
     # Вывод результатов
-    print(f"Самый дорогой заказ: {max_order_price} на сумму {max_price}")
-    print(f"Заказ с наибольшим количеством товаров: {max_order_items}, {max_items} товаров")
+    print(f"Номер самого дорогого заказа: {max_order_price} на сумму {max_price}")
+    print(f"Номер заказа с наибольшим количеством товаров: {max_order_items}, {max_items} товаров")
+    print(f"День с наибольшим количеством заказов: {most_orders_day}, {orders_per_day[most_orders_day]} заказов")
+    print(f"Пользователь с наибольшим количеством заказов: {most_orders_user}, {user_orders[most_orders_user]} заказов")
+    print(f"Пользователь с самой большой суммарной стоимостью заказов: {top_spending_user}, {user_total_spent[top_spending_user]} руб.")
     print(f"Средняя стоимость заказа: {total_price / total_orders:.2f}")
-    print(f"Среднее количество товаров в заказе: {total_items / total_orders:.2f}")
-    print(f"Количество заказов по дням: {dict(orders_per_day)}")
-    print(f"Пользователи и количество их заказов: {dict(user_orders)}")
-    print(f"Пользователи и их траты: {dict(user_total_spent)}")
+    print(f"Средняя стоимость товара: {total_price / total_items:.2f}")
+
 
 
